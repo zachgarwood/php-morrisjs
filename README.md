c20e5cbe7fdabe2d033477e1395a61da3c07761b
Zach Garwood <zachgarwood@gmail.com>
2013-12-08 23:48:12 -0600

    Tidied up, closed #1 and closed #2

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-php-morrisjs
+PHP-MorrisJS
 ============
 
-PHP wrapper for interaction with Morris.js Javascript library
+PHP library for interactiing with the [Morris.js](http://www.oesmith.co.uk/morris.js/) Javascript library
+
+[![Latest Stable Version](https://poser.pugx.org/zachgarwood/php-morrisjs/v/stable.png)](https://packagist.org/packages/zachgarwood/php-morrisjs)
+[![Build Status](https://travis-ci.org/zachgarwood/php-morris.png?branch=master)](https://travis-ci.org/zachgarwood/php-morris)
+[![Coverage Status](https://coveralls.io/repos/zachgarwood/php-morris/badge.png?branch=master)](https://coveralls.io/r/zachgarwood/php-morris?branch=master)
+[![Total Downloads](https://poser.pugx.org/zachgarwood/php-morris/downloads.png)](https://packagist.org/packages/zachgarwood/php-morris)
--- library/MorrisJs/Chart.php
+++ library/MorrisJs/Chart.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the MorrisJs package.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @author Zach Garwood <zachgarwood@gmail.com>
+ * @copyright Copyright (c) 2013 Zach Garwood
+ * @license MIT
+ */
+
 namespace MorrisJs;
 
 use DataTable\Cell;
@@ -6,41 +17,126 @@ use DataTable\Column;
 use DataTable\Row;
 use DataTable\Table;
 
+/**
+ * Chart container
+ */
 class Chart
 {
-    const
-        TYPE_AREA   = 'Area',
-        TYPE_BAR    = 'Bar',
-        TYPE_DONUT  = 'Donut',
-        TYPE_LINE   = 'Line',
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const COL_LABEL = 'label';
 
-        COL_LABEL = 'label',
-        COL_VALUE = 'value',
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const COL_VALUE = 'value';
 
-        ID_PREFIX = 'chart-',
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const ID_PREFIX = 'morris-';
 
-        RESOURCE_JQUERY = '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js',
-        RESOURCE_MORRIS_SCRIPT = '//cdn.oesmith.co.uk/morris-0.4.3.min.js',
-        RESOURCE_MORRIS_STYLESHEET = '//cdn.oesmith.co.uk/morris-0.4.3.min.css',
-        RESOURCE_RAPHAEL = '//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js';
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const RESOURCE_JQUERY = '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js';
 
-    public static
-        $stylesheets = [
-            self::RESOURCE_MORRIS_STYLESHEET,
-        ],
-        $scripts = [
-            self::RESOURCE_JQUERY,
-            self::RESOURCE_MORRIS_SCRIPT,
-            self::RESOURCE_RAPHAEL,
-        ];
+    /**
+     * @since 1.0.0
+     */
+    const RESOURCE_MORRIS_SCRIPT = '//cdn.oesmith.co.uk/morris-0.4.3.min.js';
 
-    protected
-        $data,
-        $id,
-        $options,
-        $type,
-        $xAxis;
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const RESOURCE_MORRIS_STYLESHEET = '//cdn.oesmith.co.uk/morris-0.4.3.min.css';
 
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const RESOURCE_RAPHAEL = '//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js';
+
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const TYPE_AREA = 'Area';
+
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const TYPE_BAR = 'Bar';
+
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const TYPE_DONUT = 'Donut';
+
+    /**
+     * @api
+     * @since 1.0.0
+     */
+    const TYPE_LINE = 'Line';
+
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @var string[] An array of stylesheet resource uris
+     */
+    public static $stylesheets = [
+        self::RESOURCE_MORRIS_STYLESHEET,
+    ];
+
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @var string[] An array of script resource uris
+     */
+    public static $scripts = [
+        self::RESOURCE_JQUERY,
+        self::RESOURCE_MORRIS_SCRIPT,
+        self::RESOURCE_RAPHAEL,
+    ];
+
+    /**
+     * @var DataTable\Table
+     */
+    protected $data;
+
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var string[] An array of additional options
+     */
+    protected $options;
+
+    /**
+     * @var string One of the TYPE_* constants defined in this class
+     */
+    protected $type;
+
+    /**
+     * @var DataType\Column If not manually set, the first column will be used as the X axis
+     */
+    protected $xAxis;
+
+    /**
+     * @var string[] An arry of the TYPE_* constants defined in this class
+     */
     private static
         $types = [
             self::TYPE_AREA,
@@ -49,26 +145,42 @@ class Chart
             self::TYPE_LINE,
         ];
 
-    public function __construct(
-        $type,
-        Table $data,
-        array $options = array()
-    ) {
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @param string $type
+     * @param DataTable\Table $data
+     * @param string[] $options An array of additional options
+     *
+     * @throws InvalidArgumentException if $type is not one of the constant defined in this class
+     */
+    public function __construct($type, Table $data, array $options = array()) {
         if (!in_array($type, self::$types)) {
-            throw new \InvalidArgumentException(
-                "'$type' is not a valid chart type!"
-            );
+            throw new \InvalidArgumentException("'$type' is not a valid chart type!");
         }
         $this->type = $type;
         $this->data = $data;
         $this->options = $options;
     }
 
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @return DataTable\Colum
+     */
     public function getData()
     {
         return $this->data;
     }
 
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @return string A JSON-encoded string representation of the chart
+     */
     public function convertDataToJson()
     {
         $json = new \stdClass;
@@ -102,35 +214,79 @@ class Chart
         return json_encode($json);
     }
 
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @param string $id
+     * @return string A prefixed ID
+     */
     public function setId($id)
     {
         return $this->id = self::ID_PREFIX . $id;
     }
 
+    /**
+     * @api
+     * @since 1.0.0
+     *
+     * @return string
+     */
     public function getId()
     {
-        return isset($this->id) ?
-            $this->id : $this->setId(uniqid());
+        return isset($this->id) ? $this->id : $this->setId(uniqid());
     }
 
+    /**
+     * Adds a set of options to those already set
+     *
+     * @api
+     * @since 1.0.0
+     *
+     * @param string[] $options An array of additional options
+     * @return string[] An array of all set options
+     */
     public function setOptions(array $options)
     {
         return $this->options = array_merge($this->options, $options);
     }
 
+    /**
+     * Specifies the column to be used as the X axis
+     *
+     * @api
+     * @since 1.0.0
+     *
+     * @param DataTable\Column
+     * @return DataTable\Column
+     */
     public function setXAxis(Column $xAxis)
     {
         return $this->xAxis = $xAxis;
     }
 
+    /**
+     * Returns the column used as the X axis
+     *
+     * If no column has been specified, the first column is returned.
+     *
+     * @api
+     * @since 1.0.0
+     *
+     * @return DataTable\Column
+     */
     protected function getXAxis()
     {
         $columns = $this->data->getColumns();
 
-        return isset($this->xAxis) ?
-            $this->xAxis : $this->setXAxis(reset($columns));
+        return isset($this->xAxis) ? $this->xAxis : $this->setXAxis(reset($columns));
     }
 
+    /**
+     * Returns an array to be used as the `data` property of the JSON output object
+     *
+     * @return mixed[]
+     */
     private function createDataJsonProperty()
     {
         $data = array();
@@ -138,11 +294,9 @@ class Chart
             $item = new \stdClass;
             foreach ($row->getCells() as $cell) {
                 if ($cell->getColumn()->getType() == Column::TYPE_DATETIME) {
-                    $item->{$cell->getColumn()->getId()} =
-                        $cell->value->format('Y-m-d h:i:s.u');
+                    $item->{$cell->getColumn()->getId()} = $cell->value->format('Y-m-d h:i:s.u');
                 } elseif ($cell->getColumn()->getType() == Column::TYPE_DATE) {
-                    $item->{$cell->getColumn()->getId()} =
-                        $cell->value->format('Y-m-d');
+                    $item->{$cell->getColumn()->getId()} = $cell->value->format('Y-m-d');
                 } else {
                     $item->{$cell->getColumn()->getId()} = $cell->value;
                 }
